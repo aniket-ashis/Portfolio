67a2b02deb817630115625697960f502730be543
AniketBoy <aniketashis93@gmail.com>
2024-03-05 14:39:38 +0530

    first changes

---
+++ src/components/navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar() {
+    return (
+        <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-16 lg:px-32 xl:px-64">
+            <div className="">1</div>
+            <div className="">2</div>
+        </div>
+    )
+}
+
+export default Navbar
